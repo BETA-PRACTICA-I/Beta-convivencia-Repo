320af972853074d7ccc49a959320e2bab506a34a
RGonzalez1903 <R.gonzlezmeneses@uandresbello.edu>
2025-10-01 00:40:02 -0300

    Union (validacion-protocolo1)
    Conexion de todo el avance hasta la fecha.

---
+++ Proyecto Arzo/protocolo1/urls.py
@@ -0,0 +1,30 @@
+"""formulario URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.urls import path, include
+from . import views
+
+app_name = 'protocolo1'
+
+urlpatterns = [
+    path('formulario/', views.formulario_paso1, name='formulario_paso1'),
+    path('formulario2/', views.formulario_paso2, name='formulario_paso2'),
+    #path('formulario3/', views.formulario_paso3, name='formulario_paso3'),
+    path('formulario4/', views.formulario_paso4, name='formulario_paso4'),
+    path('formulario5/', views.formulario_paso5, name='formulario_paso5'),
+    path('formulario6/', views.formulario_paso6, name='formulario_paso6'),
+    path('formulario7/', views.formulario_paso7, name='formulario_paso7'),
+]
